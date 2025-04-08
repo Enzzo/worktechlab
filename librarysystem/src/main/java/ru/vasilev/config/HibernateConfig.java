4a88643a1dc7341783ab796ea82867d4f189bcf2
enzzo <enzzomatrix@gmail.com>
2025-04-08 22:26:52 +1100

    upd

--- librarysystem/src/main/java/ru/vasilev/config/HibernateConfig.java
+++ librarysystem/src/main/java/ru/vasilev/config/HibernateConfig.java
@@ -1,27 +1,47 @@
 package ru.vasilev.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class HibernateConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
-    public LocalSessionFactoryBean sessionFactory() {
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        // Указываем расположение конфигурационного файла Hibernate
-        sessionFactory.setConfigLocation(new ClassPathResource("hibernate.cfg.xml"));
+        sessionFactory.setDataSource(dataSource);
+        
+        // Указываем пакет(ы) с сущностями
+        sessionFactory.setPackagesToScan("com.example.demo.entity");
+        
+        // Настройки Hibernate (берутся из application.properties и профилей)
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", env.getProperty("spring.jpa.database-platform"));
+        hibernateProperties.put("hibernate.show_sql", env.getProperty("spring.jpa.show-sql"));
+        hibernateProperties.put("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.hibernate.ddl-auto"));
+        sessionFactory.setHibernateProperties(hibernateProperties);
+        
         return sessionFactory;
     }
 
     @Bean
     public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-       HibernateTransactionManager txManager = new HibernateTransactionManager();
-       txManager.setSessionFactory(sessionFactory);
-       return txManager;
+       HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+       transactionManager.setSessionFactory(sessionFactory);
+       return transactionManager;
     }
 }
