0a1267c28ce157b82ca42fa7349ad637a37b6bac
unknown <enzzomatrix@gmail.com>
2025-06-25 10:04:32 +1100

    upd

--- librarysystem/src/main/java/ru/vasilev/LibrarysystemApplication.java
+++ librarysystem/src/main/java/ru/vasilev/LibrarysystemApplication.java
@@ -2,10 +2,12 @@ package ru.vasilev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableJpaRepositories
 public class LibrarysystemApplication {
 
 	public static void main(String[] args) {
